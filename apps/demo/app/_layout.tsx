ebbc2d7425bba199dfafbed6bb3dcb0595e4cb17
Enoch Klu <eenoch@delalify.com>
2025-01-18 14:12:21 +0000

    refactor: move navigation components and providers

--- apps/demo/app/_layout.tsx
+++ apps/demo/app/_layout.tsx
@@ -1,19 +1,13 @@
-import { ThemeProvider as NativeThemeProvider } from '@react-navigation/native'
 import { useFonts } from 'expo-font'
-import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
-import { StatusBar } from 'expo-status-bar'
 import { useEffect } from 'react'
 import 'react-native-reanimated'
-import { ThemeProvider, useThemeMode } from 'bleach'
-import { getNativeTheme } from '../utilities'
-import { Themes } from '../theme'
+import MainLayout from '../components/app/MainLayout'
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const { mode } = useThemeMode()
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   })
@@ -28,15 +22,5 @@ export default function RootLayout() {
     return null
   }
 
-  return (
-    <ThemeProvider themes={Themes}>
-      <NativeThemeProvider value={getNativeTheme(mode === 'dark' ? Themes.dark : Themes.light)}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-        <StatusBar style="auto" />
-      </NativeThemeProvider>
-    </ThemeProvider>
-  )
+  return <MainLayout />
 }
---
+++ apps/demo/components/app/MainLayout.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider as NativeThemeProvider } from '@react-navigation/native'
+import { StatusBar } from 'expo-status-bar'
+import { ThemeProvider, useThemeMode } from 'bleach'
+import { getNativeTheme } from '../../utilities'
+import { Themes } from '../../theme'
+import ScreenStack from './ScreenStack'
+
+export default function MainLayout() {
+  const { mode } = useThemeMode()
+
+  return (
+    <ThemeProvider themes={Themes}>
+      <NativeThemeProvider value={getNativeTheme(mode === 'dark' ? Themes.dark : Themes.light)}>
+        <ScreenStack />
+        <StatusBar style="auto" />
+      </NativeThemeProvider>
+    </ThemeProvider>
+  )
+}
