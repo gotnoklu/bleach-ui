9db7f0cdbe7af1bce176a8d8d831f66c9f7525ac
Enoch Klu <eenoch@delalify.com>
2025-01-18 11:49:04 +0000

    refactor: set up theme provider with new themes

--- apps/demo/app/_layout.tsx
+++ apps/demo/app/_layout.tsx
@@ -1,23 +1,19 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider as NativeThemeProvider,
-} from '@react-navigation/native'
+import { ThemeProvider as NativeThemeProvider } from '@react-navigation/native'
 import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
 import { StatusBar } from 'expo-status-bar'
 import { useEffect } from 'react'
 import 'react-native-reanimated'
-
-import { useColorScheme } from '@/hooks/useColorScheme'
-import { ThemeProvider } from 'bleach'
+import { ThemeProvider, useThemeMode } from 'bleach'
+import { getNativeTheme } from '../utilities'
+import { Themes } from '../theme'
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme()
+  const { mode } = useThemeMode()
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   })
@@ -33,8 +29,8 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider>
-      <NativeThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider themes={Themes}>
+      <NativeThemeProvider value={getNativeTheme(mode === 'dark' ? Themes.dark : Themes.light)}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
