5a579dc15629b10c7427a64e1b0946e3a61c1d56
Enoch Klu <eenoch@delalify.com>
2025-01-19 15:35:24 +0000

    feat: enable creation of any icon component

--- packages/bleach/src/components/Icon/index.tsx
+++ packages/bleach/src/components/Icon/index.tsx
@@ -4,23 +4,28 @@ import type { OcticonsName } from './types'
 import type { Palette, SxProps } from '../../theme/types'
 import type { TextProps } from 'react-native'
 import { useTheme } from '../../theme/hooks'
+import { type ComponentType, createElement } from 'react'
 
 export interface IconProps extends TextProps, SxProps<TextProps> {
   name: OcticonsName
   size?: number
   color?: keyof Palette | (string & {})
+  component?: ComponentType<{ name?: string; size?: number; color?: string } & TextProps>
 }
 
-export default function Icon({ name, size = 22.5, color = 'action', style, ...props }: IconProps) {
+export default function Icon({
+  name,
+  size = 22.5,
+  color = 'action',
+  component,
+  ...props
+}: IconProps) {
   const theme = useTheme()
+  const iconColor = getThemeProperty({ object: theme.palette, key: color, fallback: color })
 
-  return (
-    <Octicons
-      name={name}
-      size={size}
-      style={[style]}
-      color={getThemeProperty({ object: theme.palette, key: color, fallback: color })}
-      {...props}
-    />
-  )
+  if (component) {
+    return createElement(component, { name, size, color: iconColor, ...props })
+  }
+
+  return <Octicons name={name} size={size} color={iconColor} {...props} />
 }
