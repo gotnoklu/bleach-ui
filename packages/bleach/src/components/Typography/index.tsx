237135dd1be8adb955f0d7c900256a105040d5c8
Enoch Klu <eenoch@delalify.com>
2025-01-19 11:04:21 +0000

    fix: font application for typography component

--- packages/bleach/src/components/Typography/index.tsx
+++ packages/bleach/src/components/Typography/index.tsx
@@ -1,19 +1,18 @@
 import type { TextProps } from 'react-native'
 import { Text } from 'react-native'
 import { getThemeProperty, merge, styled } from '../../theme/utilities'
-import type { SxProps, TextColor, TypographyVariants, TypographyWeights } from '../../theme/types'
+import type { SxProps, TextColor, TypographyVariants, TypographyTypes } from '../../theme/types'
 
 export interface TypographyProps extends TextProps, SxProps<TextProps> {
   variant?: keyof TypographyVariants
   color?: keyof TextColor | (string & {})
-  fontWeight?: keyof TypographyWeights
+  fontWeight?: keyof TypographyTypes
   gutterBottom?: boolean
 }
 
 const StyledTypography = styled(Text)<TypographyProps>(
   (theme, { variant = 'body1', color = 'text.primary', fontWeight = 'regular', gutterBottom }) => {
-    return merge(theme.typography.weights[fontWeight], theme.typography.variants[variant], {
-      fontFamily: theme.typography.weights[fontWeight].fontFamily,
+    return merge(theme.typography.types[fontWeight], theme.typography.variants[variant], {
       color: getThemeProperty({ object: theme.palette, key: color, fallback: color }),
       marginBottom: gutterBottom ? theme.spacing.create(1) : 0,
     })
