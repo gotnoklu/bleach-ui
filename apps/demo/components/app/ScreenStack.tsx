b1e2705fb1b8f25015d3a757f2bd393a9a94b1ab
Enoch Klu <eenoch@delalify.com>
2025-01-19 13:59:57 +0000

    refactor: remove tabs layout

--- apps/demo/components/app/ScreenStack.tsx
+++ apps/demo/components/app/ScreenStack.tsx
@@ -1,9 +1,22 @@
+import { useTheme } from 'bleach'
 import { Stack } from 'expo-router'
 
 export default function ScreenStack() {
+  const theme = useTheme()
+
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: 'Welcome',
+          headerTitle: 'Welcome',
+          headerTitleStyle: {
+            color: theme.palette.text.primary,
+            ...(theme.typography.types.medium as { fontFamily: string }),
+          },
+        }}
+      />
       <Stack.Screen name="+not-found" />
     </Stack>
   )
