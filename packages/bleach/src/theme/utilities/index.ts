9f7d2406639212a6d009ad8bcf427e817d965e38
assetcorp <sondelalimic@gmail.com>
2025-01-19 13:00:08 +0000

    feat: crate an `alpha` utility to calculate color opacity

--- packages/bleach/src/theme/utilities/index.ts
+++ packages/bleach/src/theme/utilities/index.ts
@@ -181,3 +181,19 @@ export function selectStyles<
 
   return merge(...result.filter((style) => style !== null)) as TStyles[number]['styles']
 }
+
+export function alpha(color: string, opacity: number): string {
+  if (color.startsWith('rgba')) {
+    return color.replace(/[\d.]+\)$/g, `${opacity})`)
+  }
+  if (color.startsWith('rgb')) {
+    return color.replace('rgb', 'rgba').replace(')', `, ${opacity})`)
+  }
+  if (color.startsWith('#')) {
+    const r = Number.parseInt(color.slice(1, 3), 16)
+    const g = Number.parseInt(color.slice(3, 5), 16)
+    const b = Number.parseInt(color.slice(5, 7), 16)
+    return `rgba(${r}, ${g}, ${b}, ${opacity})`
+  }
+  return color
+}
