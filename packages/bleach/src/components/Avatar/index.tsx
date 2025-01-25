3f52a8c90c5fa8e3707d4a4be856f751b009a49e
assetcorp <sondelalimic@gmail.com>
2025-01-25 06:45:12 +0000

    feat: create avatar component

---
+++ packages/bleach/src/components/Avatar/index.tsx
@@ -0,0 +1,94 @@
+import { Image, type ImageProps, View, type ViewProps } from 'react-native'
+import { styled, selectStyles, getThemeProperty } from '../../theme/utilities'
+import type { Palette, SxProps, TextColor } from '../../theme/types'
+import Typography, { type TypographyProps } from '../Typography'
+import type { ReactNode } from 'react'
+
+export type AvatarVariant = 'circular' | 'rounded' | 'square'
+export type AvatarSize = 'small' | 'medium' | 'large'
+
+export interface AvatarProps extends ViewProps, SxProps<ViewProps> {
+  variant?: AvatarVariant
+  size?: AvatarSize
+  src?: string
+  alt?: string
+  color?: keyof Palette | keyof TextColor | (string & {})
+  backgroundColor?: keyof Palette | (string & {})
+  children?: ReactNode
+  slotProps?: {
+    container?: ViewProps
+    image?: Omit<ImageProps, 'source'>
+    text?: TypographyProps
+  }
+}
+
+const StyledAvatar = styled(View)<Omit<AvatarProps, 'sx'>>(
+  (theme, { variant = 'circular', size = 'medium', backgroundColor = 'action' }) => {
+    const avatarSizes = { small: 32, medium: 40, large: 48 }
+    const bgColor = getThemeProperty({
+      object: theme.palette,
+      key: backgroundColor,
+      fallback: theme.palette.action,
+    })
+
+    return selectStyles(
+      {
+        if: variant === 'circular',
+        styles: { borderRadius: avatarSizes[size] / 2 },
+      },
+      {
+        if: variant === 'rounded',
+        styles: { borderRadius: theme.radius.create(1) },
+      },
+      {
+        if: variant === 'square',
+        styles: { borderRadius: 0 },
+      },
+      {
+        styles: {
+          backgroundColor: bgColor,
+          width: avatarSizes[size],
+          height: avatarSizes[size],
+          alignItems: 'center',
+          justifyContent: 'center',
+          overflow: 'hidden',
+        },
+      }
+    )
+  }
+)
+
+const AvatarImage = ({ source, ...props }: ImageProps) => (
+  <Image source={source} style={{ width: '100%', height: '100%' }} {...props} />
+)
+
+export default function Avatar({
+  src,
+  alt,
+  color = 'text.primary',
+  children,
+  slotProps,
+  ...props
+}: AvatarProps) {
+  const renderContent = () => {
+    if (src) {
+      return <AvatarImage source={{ uri: src }} resizeMode="cover" {...slotProps?.image} />
+    }
+
+    if (typeof children === 'string') {
+      return (
+        <Typography variant="body2" color={color} {...slotProps?.text}>
+          {children.charAt(0).toUpperCase()}
+        </Typography>
+      )
+    }
+
+    return children
+  }
+
+  return (
+    <StyledAvatar {...slotProps?.container} {...props}>
+      {renderContent()}
+    </StyledAvatar>
+  )
+}
