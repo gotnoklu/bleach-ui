73258e71baf696233b966ff263f326e1210e7bf0
Enoch Klu <eenoch@delalify.com>
2025-01-26 15:06:39 +0000

    refactor: add styling props to box component

--- packages/bleach/src/components/Box/index.tsx
+++ packages/bleach/src/components/Box/index.tsx
@@ -4,11 +4,45 @@ import type { SxProps } from '../../theme/types'
 
 export interface BoxProps extends ViewProps, SxProps<ViewProps> {
   row?: boolean
+  gap?: number
+  padding?: number
+  margin?: number
+  paddingX?: number
+  marginX?: number
+  paddingY?: number
+  marginY?: number
+  alignItems?: 'flex-start' | 'flex-end' | 'center'
+  justifyContent?: 'flex-start' | 'flex-end' | 'center'
 }
 
-const StyledBox = styled(View)<Omit<BoxProps, 'sx'>>((_theme, { row }) => ({
-  flexDirection: row ? 'row' : 'column',
-}))
+const StyledBox = styled(View)<Omit<BoxProps, 'sx'>>(
+  (
+    theme,
+    {
+      row,
+      alignItems,
+      justifyContent,
+      padding = 0,
+      paddingY = 0,
+      paddingX = 0,
+      margin = 0,
+      marginY = 0,
+      marginX = 0,
+      gap = 0,
+    }
+  ) => ({
+    flexDirection: row ? 'row' : 'column',
+    padding: theme.spacing.create(padding),
+    margin: theme.spacing.create(margin),
+    paddingVertical: theme.spacing.create(paddingY),
+    marginVertical: theme.spacing.create(marginY),
+    paddingHorizontal: theme.spacing.create(paddingX),
+    marginHorizontal: theme.spacing.create(marginX),
+    alignItems: alignItems,
+    justifyContent: justifyContent,
+    gap: theme.spacing.create(gap),
+  })
+)
 
 export default function Box({ children, ...props }: BoxProps) {
   return <StyledBox {...props}>{children}</StyledBox>
