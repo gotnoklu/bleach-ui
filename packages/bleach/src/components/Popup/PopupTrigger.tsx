8ae4196dd0971851484d266953b37ed64b26f136
Enoch Klu <eenoch@delalify.com>
2025-02-02 10:42:52 +0000

    fix: pass all props

--- packages/bleach/src/components/Popup/PopupTrigger.tsx
+++ packages/bleach/src/components/Popup/PopupTrigger.tsx
@@ -6,29 +6,30 @@ import {
   forwardRef,
   isValidElement,
 } from 'react'
-import { Pressable, type View } from 'react-native'
+import { Pressable, type ViewProps, type View } from 'react-native'
 import { usePopupContext } from './context'
 
-export interface PopupTriggerProps {
+export interface PopupTriggerProps extends ViewProps {
   triggerOnEvent?: 'onPress' | 'onPressIn' | 'onPressOut' | 'onLongPress'
   children: ReactNode
 }
 
 const PopupTrigger = forwardRef(function PopupTrigger(
-  { triggerOnEvent = 'onPress', children }: PopupTriggerProps,
+  { triggerOnEvent = 'onPress', children, ...props }: PopupTriggerProps,
   ref: ForwardedRef<View>
 ) {
   const { onShow } = usePopupContext()
 
   if (isValidElement(children)) {
     return cloneElement(children as Exclude<typeof children, ReactElement>, {
+      ...props,
       ref,
       [triggerOnEvent]: onShow,
     })
   }
 
   return (
-    <Pressable ref={ref} {...{ [triggerOnEvent]: onShow }}>
+    <Pressable {...{ ...props, [triggerOnEvent]: onShow }} ref={ref}>
       {children}
     </Pressable>
   )
