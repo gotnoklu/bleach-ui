feb0d6ce039c9fce1ed7e8cb3245017e9e32b0df
Enoch Klu <eenoch@delalify.com>
2025-03-23 11:32:17 +0000

    feat: add paper variant

--- packages/bleach/src/components/Paper/index.tsx
+++ packages/bleach/src/components/Paper/index.tsx
@@ -1,17 +1,30 @@
 import { View, type ViewProps } from 'react-native'
-import { styled } from '../../theme/utilities'
+import { selectStyles, styled } from '../../theme/utilities'
 import type { SxProps } from '../../theme/types'
 import { type ForwardedRef, forwardRef } from 'react'
 
 export interface PaperProps extends ViewProps, SxProps<ViewProps> {
+  variant?: 'outlined' | 'elevated'
   roundness?: number
 }
 
-const StyledPaper = styled(View)<Omit<PaperProps, 'sx'>>((theme, { roundness = 2 }) => ({
-  borderRadius: theme.radius.create(roundness),
-  backgroundColor: theme.palette.backgrounds.paper,
-  ...theme.shadows[1],
-}))
+const StyledPaper = styled(View, { omitProps: ['variant'] })<Omit<PaperProps, 'sx'>>(
+  (theme, { variant = 'elevated', roundness = 2 }) => {
+    return selectStyles(
+      { if: variant === 'elevated' || variant === undefined, styles: theme.shadows[1] },
+      {
+        if: variant === 'outlined',
+        styles: { borderColor: theme.palette.divider, borderWidth: 1 },
+      },
+      {
+        styles: {
+          borderRadius: theme.radius.create(roundness),
+          backgroundColor: theme.palette.backgrounds.paper,
+        },
+      }
+    )
+  }
+)
 
 const Paper = forwardRef(function Paper(props: PaperProps, ref: ForwardedRef<View>) {
   return <StyledPaper ref={ref} {...props} />
