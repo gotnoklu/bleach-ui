9fc7ba792afbdf017188da4e265bd47f6c2fa710
Enoch Klu <eenoch@delalify.com>
2025-12-09 15:57:15 +0000

    refactor: update box showcase

--- apps/demo/layouts/showcases/box.tsx
+++ apps/demo/layouts/showcases/box.tsx
@@ -6,16 +6,50 @@ export const BoxShowcase = () => {
   return (
     <Section>
       <Text variant="h5">Boxes</Text>
-      <SubSection>
-        <Text variant="h6" color="text.secondary">
-          Row
-        </Text>
-        <Box direction="row" gap={2}>
-          <Box backgroundColor="primary" width={50} height={50} />
-          <Box backgroundColor="primary" width={50} height={50} />
-          <Box backgroundColor="primary" width={50} height={50} />
-        </Box>
-      </SubSection>
+      <Box direction="row">
+        <SubSection style={{ flex: 1 }}>
+          <Text variant="h6" color="text.secondary">
+            Row
+          </Text>
+          <Box direction="row" gap={2}>
+            <Box backgroundColor="primary" width={32} height={32} borderRadius={4} />
+            <Box backgroundColor="error" width={32} height={32} borderRadius={4} />
+            <Box backgroundColor="success" width={32} height={32} borderRadius={4} />
+          </Box>
+        </SubSection>
+        <SubSection style={{ flex: 1 }}>
+          <Text variant="h6" color="text.secondary" textAlign="right">
+            Row Reverse
+          </Text>
+          <Box direction="row-reverse" gap={2}>
+            <Box backgroundColor="primary" width={32} height={32} borderRadius={4} />
+            <Box backgroundColor="error" width={32} height={32} borderRadius={4} />
+            <Box backgroundColor="success" width={32} height={32} borderRadius={4} />
+          </Box>
+        </SubSection>
+      </Box>
+      <Box direction="row">
+        <SubSection style={{ flex: 1 }}>
+          <Text variant="h6" color="text.secondary">
+            Column
+          </Text>
+          <Box direction="column" gap={2}>
+            <Box backgroundColor="primary" width={32} height={32} borderRadius={4} />
+            <Box backgroundColor="error" width={32} height={32} borderRadius={4} />
+            <Box backgroundColor="success" width={32} height={32} borderRadius={4} />
+          </Box>
+        </SubSection>
+        <SubSection style={{ flex: 1 }}>
+          <Text variant="h6" color="text.secondary">
+            Column Reverse
+          </Text>
+          <Box direction="column-reverse" gap={2}>
+            <Box backgroundColor="primary" width={32} height={32} borderRadius={4} />
+            <Box backgroundColor="error" width={32} height={32} borderRadius={4} />
+            <Box backgroundColor="success" width={32} height={32} borderRadius={4} />
+          </Box>
+        </SubSection>
+      </Box>
     </Section>
   )
 }
